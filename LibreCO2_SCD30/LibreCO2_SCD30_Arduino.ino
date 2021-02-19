149dda5cd9e9eb9d60aca4c430daf9b65e8a9e64
danielbernalb <danielbernalb@gmail.com>
2021-02-19 17:37:41 -0500

    Rename routines form CM1106 to SCD30

--- LibreCO2_SCD30/LibreCO2_SCD30_Arduino.ino
+++ LibreCO2_SCD30/LibreCO2_SCD30_Arduino.ino
@@ -1,29 +1,25 @@
 /*
-  Reading CO2, humidity and temperature from the SCD30, visualize in TM1637 7 segments, buzzer, calibration routine.
-  Modbus serial mode
+  Reading CO2 from the CM1106, visualize in TM1637 7 segments, buzzer, calibration routine.
+  Routine of CM1106 lecture based on agnunez/moco2 repository
   By: Daniel Bernal
-  Date: Feb 2, 2021
+  Date: Feb 7, 2021
 
   Hardware Connections:
-  SCD30 VCC to 3.3V
-  SEL pin to 3.3V
-  SCD30 with pins connected to 6 and 7 pin
-  TM1637 board connected at pin 8 and 9
-  Buzzer connected at pin 11
-  Button connected at pin 2
+  CM1106 with pins to A5 and A4
+  TM1637 board connected at pin A2 and A0
+  Buzzer connected at pin 8
+  Button connected at pin 12
 
-  Leyendo CO2, humedad y temeratura desde el SCD30 marca Sensirion, visualización en el 7 segmentos TM1637, chicharra y rutina de calibración.
-  Protoloco Modbus serial
+  Leyendo CO2 desde el sensor CM1106 marca Cubic, visualización en el 7 segmentos TM1637, chicharra y rutina de calibración.
+  Rutina de lectura del sensor basada en el respositorio agnunez/moco2
   Por: Daniel Bernal
-  Fecha: Feb 2, 2021
+  Fecha: Feb 7, 2021
 
-  Conexión de Hardware:
-  SCD30 alimentacion a 3.3V
-  Pin SEL a 3.3V
-  Sensor SCD30 con pines conectados a 6 y 7
-  Board TM1637 conectada a los pines 8 y 9
-  Chicharra conectada al pin 11
-  Boton o cable conectado a pin 2
+  Conección de Hardware:
+  Sensor CM1106 conectado a A5 y A4
+  Board TM1637 conectada a los pines A2 y A0
+  Chicharra conectada al pin 8
+  Boton o cable conectado a pin 12
 */
 
 const byte PIN_TX = 6;   // define CLK pin (any digital pin)
@@ -48,7 +44,7 @@ unsigned char buffer[4];
 #include "SevenSegmentTM1637.h"
 #include "SevenSegmentExtended.h"
 SevenSegmentExtended display(PIN_CLK, PIN_DIO);
-SoftwareSerial co2CM(PIN_RX, PIN_TX);
+SoftwareSerial co2SCD(PIN_RX, PIN_TX);
 
 
 void setup()
@@ -60,7 +56,7 @@ void setup()
   Serial.println("Start SCD30 Modbus lecture");
   display.begin();            // initializes the display
   display.setBacklight(100);  // set the brightness to 100 %
-  co2CM.begin(BAUDRATE);      // (Uno example) device to CM1106 serial start
+  co2SCD.begin(BAUDRATE);      // (Uno example) device to CM1106 serial start
   delay(1000);
 
   // Connection test
@@ -70,26 +66,12 @@ void setup()
 
   static byte cmd[8] = {0x61, 0x06, 0x00, 0x36, 0x00, 0x00, 0x60, 0x64};
   //response = {0};
-  co2CM.write(cmd, 8);
-  co2CM.readBytes(response, 8);
+  co2SCD.write(cmd, 8);
+  co2SCD.readBytes(response, 8);
 
   Serial.println("Start continious measurement");
-  Serial.print(response[0], HEX);
-  Serial.print(" ");
-  Serial.print(response[1], HEX);
-  Serial.print(" ");
-  Serial.print(response[2], HEX);
-  Serial.print(" ");
-  Serial.print(response[3], HEX);
-  Serial.print(" ");
-  Serial.print(response[4], HEX);
-  Serial.print(" ");
-  Serial.print(response[5], HEX);
-  Serial.print(" ");
-  Serial.print(response[6], HEX);
-  Serial.print(" ");
-  Serial.print(response[7], HEX);
-  Serial.println();
+  Serial.print(response[0], HEX); Serial.print(" "); Serial.print(response[1], HEX); Serial.print(" "); Serial.print(response[2], HEX); Serial.print(" "); Serial.print(response[3], HEX); Serial.print(" ");
+  Serial.print(response[4], HEX); Serial.print(" "); Serial.print(response[5], HEX); Serial.print(" "); Serial.print(response[6], HEX); Serial.print(" "); Serial.print(response[7], HEX);  Serial.println();
 
   delay(1000);
 
@@ -99,26 +81,12 @@ void setup()
 
   static byte cmd2[8] = {0x61, 0x06, 0x00, 0x25, 0x00, 0x02, 0x10, 0x60};
   //response = {0};
-  co2CM.write(cmd2, 8);
-  co2CM.readBytes(response, 8);
+  co2SCD.write(cmd2, 8);
+  co2SCD.readBytes(response, 8);
 
   Serial.println("Set measurement interval 2 seconds ");
-  Serial.print(response[0], HEX);
-  Serial.print(" ");
-  Serial.print(response[1], HEX);
-  Serial.print(" ");
-  Serial.print(response[2], HEX);
-  Serial.print(" ");
-  Serial.print(response[3], HEX);
-  Serial.print(" ");
-  Serial.print(response[4], HEX);
-  Serial.print(" ");
-  Serial.print(response[5], HEX);
-  Serial.print(" ");
-  Serial.print(response[6], HEX);
-  Serial.print(" ");
-  Serial.print(response[7], HEX);
-  Serial.println();
+  Serial.print(response[0], HEX); Serial.print(" "); Serial.print(response[1], HEX); Serial.print(" "); Serial.print(response[2], HEX); Serial.print(" "); Serial.print(response[3], HEX); Serial.print(" ");
+  Serial.print(response[4], HEX); Serial.print(" "); Serial.print(response[5], HEX); Serial.print(" "); Serial.print(response[6], HEX); Serial.print(" "); Serial.print(response[7], HEX); Serial.println();
 
   delay(1000);
 
@@ -128,26 +96,12 @@ void setup()
 
   static byte cmd3[8] = {0x61, 0x06, 0x00, 0x3A, 0x00, 0x00, 0xA0, 0x67};
   //response = {0};
-  co2CM.write(cmd3, 8);
-  co2CM.readBytes(response, 8);
+  co2SCD.write(cmd3, 8);
+  co2SCD.readBytes(response, 8);
 
   Serial.println("Deactivate Automatic Self-Calibration ");
-  Serial.print(response[0], HEX);
-  Serial.print(" ");
-  Serial.print(response[1], HEX);
-  Serial.print(" ");
-  Serial.print(response[2], HEX);
-  Serial.print(" ");
-  Serial.print(response[3], HEX);
-  Serial.print(" ");
-  Serial.print(response[4], HEX);
-  Serial.print(" ");
-  Serial.print(response[5], HEX);
-  Serial.print(" ");
-  Serial.print(response[6], HEX);
-  Serial.print(" ");
-  Serial.print(response[7], HEX);
-  Serial.println();
+  Serial.print(response[0], HEX); Serial.print(" "); Serial.print(response[1], HEX); Serial.print(" "); Serial.print(response[2], HEX); Serial.print(" "); Serial.print(response[3], HEX); Serial.print(" ");
+  Serial.print(response[4], HEX); Serial.print(" "); Serial.print(response[5], HEX); Serial.print(" "); Serial.print(response[6], HEX); Serial.print(" "); Serial.print(response[7], HEX); Serial.println();
 
   delay(1000);
 
@@ -156,24 +110,12 @@ void setup()
 
   static byte cmd4[8] = {0x61, 0x03, 0x00, 0x3A, 0x00, 0x01, 0xAD, 0xA7};
   //response = {0};
-  co2CM.write(cmd4, 8);
-  co2CM.readBytes(response, 7);
+  co2SCD.write(cmd4, 8);
+  co2SCD.readBytes(response, 7);
 
   Serial.println("Autocalibration");
-  Serial.print(response[0], HEX);
-  Serial.print(" ");
-  Serial.print(response[1], HEX);
-  Serial.print(" ");
-  Serial.print(response[2], HEX);
-  Serial.print(" ");
-  Serial.print(response[3], HEX);
-  Serial.print(" ");
-  Serial.print(response[4], HEX);
-  Serial.print(" ");
-  Serial.print(response[5], HEX);
-  Serial.print(" ");
-  Serial.print(response[6], HEX);
-  Serial.println();
+  Serial.print(response[0], HEX); Serial.print(" "); Serial.print(response[1], HEX); Serial.print(" "); Serial.print(response[2], HEX); Serial.print(" "); Serial.print(response[3], HEX); Serial.print(" ");
+  Serial.print(response[4], HEX); Serial.print(" "); Serial.print(response[5], HEX); Serial.print(" "); Serial.print(response[6], HEX); Serial.println();
 //  Serial.print(response[7], HEX);
 //  Serial.println();
   
@@ -190,8 +132,8 @@ void setup()
 
     static byte cmd3[8] = {0x61, 0x03, 0x00, 0x20, 0x00, 0x01, 0x8C, 0x60};
     static byte response3[7] = {0};
-    co2CM.write(cmd3, 8);
-    co2CM.readBytes(response3, 7);
+    co2SCD.write(cmd3, 8);
+    co2SCD.readBytes(response3, 7);
 
     Serial.println("Read Version");
     Serial.println(response3[0], HEX);
@@ -208,8 +150,8 @@ void setup()
 
     static byte cmd2[8] = {0x61, 0x03, 0x00, 0x28, 0x00, 0x06, 0x4C, 0x60};
     static byte response2[17] = {0};
-    co2CM.write(cmd2, 8);
-    co2CM.readBytes(response2, 17);
+    co2SCD.write(cmd2, 8);
+    co2SCD.readBytes(response2, 17);
 
     Serial.println("Read Sensor");
     Serial.print(response2[0], HEX);
@@ -277,22 +219,18 @@ void setup()
     Serial.println(tempU32);
     Serial.println(co2Concentration);
 
-
-
-  }
-
 }
 
 
-  if (co2CM1106() == 0) {
+  if (co2SCD30() == 0) {
     Serial.println("Air sensor not detected. Please check wiring...");
     display.print("bad");
     delay(5000);
-    if (co2CM1106() == 0) {
+    if (co2SCD30() == 0) {
       Serial.println("Air sensor not detected. Please check wiring...");
       display.print("bad");
       delay(5000);
-      if (co2CM1106() == 0) {
+      if (co2SCD30() == 0) {
         Serial.println("Air sensor not detected. Please check wiring");
         Serial.println("Freezed.....RESET the Arduino");
         display.print("bad-");
@@ -326,7 +264,7 @@ void setup()
 
 void loop()
 {
-  CO2 = co2CM1106();
+  CO2 = co2SCD30();
 
   if (CO2 > 0) {
     Serial.print("CO2(ppm): ");
@@ -362,7 +300,7 @@ void loop()
         for (int i = 300; i > -1; i--) { // loop from 0 to 300
           display.printNumber(i);
           Serial.print(i);
-          CO2 = co2CM1106();
+          CO2 = co2SCD30();
           Serial.print(" CO2(ppm): ");
           Serial.println(CO2);
           delay(1000);
@@ -378,11 +316,11 @@ void loop()
   }
 }
 
-int co2CM1106() {
+int co2SCD30() {
   static byte cmd[4] = {0x11, 0x01, 0x01, 0xED}; // Commands 0x01 Read ppm, 0x10 open/close ABC, 0x03 Calibrate
   static byte response[8] = {0};                 // response 0x16, 0x05, 0x01, DF1, DF2, DF3, DF4, CRC.  ppm=DF1*256+DF2
-  co2CM.write(cmd, 4);
-  co2CM.readBytes(response, 8);
+  co2SCD.write(cmd, 4);
+  co2SCD.readBytes(response, 8);
   int crc = 0;
   for (int i = 0; i < 7; i++) crc += response[i];
   crc = 256 - crc % 256;
@@ -391,7 +329,7 @@ int co2CM1106() {
     unsigned int responseLow = (unsigned int) response[4];
     return (256 * responseHigh) + responseLow;
   } else {
-    while (co2CM.available() > 0)  char t = co2CM.read(); // Clear serial input buffer;
+    while (co2SCD.available() > 0)  char t = co2SCD.read(); // Clear serial input buffer;
     return 0;
   }
 }
@@ -410,8 +348,8 @@ void Calibration()
   Serial.print("Resetting forced calibration factor to 400: ");
   static byte cmd[6] = {0x11, 0x03, 0x03, 0x01, 0x90, 0x58}; // Command Calibration of CO2 Concentration to 400ppm
   static byte response[4] = {0};
-  co2CM.write(cmd, 6);
-  co2CM.readBytes(response, 4);
+  co2SCD.write(cmd, 6);
+  co2SCD.readBytes(response, 4);
   if (response[0] == 0x16 && response[1] == 0x01 && response[2] == 0x03 && response[3] == 0xE6) {
     Serial.println("done");
     display.clear();
